6640c4faada1cd879af5e0346c4037adbf682228
DaylightDragon <work.daylight@gmail.com>
2025-09-15 16:20:35 +0300

    (Not 1.0.0!) Something for multiloader support (Work in progress)

--- forge/src/main/java/org/daylight/coinscalculator/References.java
+++ forge/src/main/java/org/daylight/coinscalculator/References.java
@@ -4,10 +4,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.IdentityHashMap;
-import java.util.List;
-import java.util.Map;
-
 public class References {
 
 }
