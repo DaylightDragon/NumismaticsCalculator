d37b88d8e2122502c607a8d4929ac3bdc9104887
DaylightDragon <work.daylight@gmail.com>
2025-09-28 01:24:13 +0300

    Basic NeoForge, btw Forge won't work anymore

---
+++ neoforge/src/main/java/org/daylight/numismaticscalculator/neoforge/NeoForgeMod.java
@@ -0,0 +1,12 @@
+package org.daylight.numismaticscalculator.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(NeoForgeMod.MOD_ID)
+public final class NeoForgeMod {
+    public static final String MOD_ID = "numismaticscalculator";
+    public NeoForgeMod() {
+        // Run our common setup.
+//        Neoforgesources1211.init();
+    }
+}
